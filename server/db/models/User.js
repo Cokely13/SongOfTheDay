ff0f9bcbb7730ff5a136c25bc25dfa06b89c4ec3
Ryan Cokely <ryan.cokely@gmail.com>
2024-02-10 12:44:32 -0500

    troubleshoot

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 // const axios = require('axios');
 
 const SALT_ROUNDS = 5;
